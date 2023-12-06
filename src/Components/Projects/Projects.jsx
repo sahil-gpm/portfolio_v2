dd64d83d1cc31d76a5f5bbafcd48df4060df8855
sahil <sahil@sahils-MacBook-Air.local>
2023-12-07 02:12:25 +0530

    changes applied in width of about section 8th time along with github links and little more changes 5th  time

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -34,7 +34,7 @@ export const Projects = () => {
     }, ]
 
     return (
-        <motion.div className='mt-20 w-[90%] md:w-[65%] mx-auto text-center'
+        <motion.div className='mt-20 w-[90%] md:w-[85%] mx-auto text-center'
             ref={ref}
             variants={{
                 hidden: { opacity: 0, y: 75 },
